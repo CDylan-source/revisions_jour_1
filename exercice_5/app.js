7a185bd6a3eeb00c97dea42cb7ff88b70ae9ee13
“CDylan-source” <chapuis.dylan18@gmail.com>
2021-09-13 16:31:05 +0200

    Suite de l'exercice 5

--- exercice_5/app.js
+++ exercice_5/app.js
@@ -10,11 +10,41 @@ square.addEventListener('click', function(){
     square.classList = "round";
 });
 
+
 const dice = document.querySelector('#multiple_events button');
 let films = Array('Matrix', 'Terminator 2', "Légendes d'automne", 'Mr Nobody', 'La vie rêvée de Walter Mitty');
 console.log(films);
 function choice(array, element){
-    let lenght = array.lenght;
-    let number = Math.floor(Math.random * lenght);
+    let length = array.length;
+    let number = Math.floor(Math.random() * length);
     element.innerHTML = array[number];
 }
+const results = document.querySelector('#multiple_events p')
+dice.addEventListener('click', function(e){
+    choice(films, results)});
+
+
+const input = document.querySelector('#value input');
+const button = document.querySelector('#value button');
+const p = document.querySelector('#value p');
+const min = new RegExp('[a-zéàùêâûôèîçäëïüö]');
+const maj = new RegExp('[A-ZÉÀÙÊÂÛÔÎÈÇÄËÏÜÖ]');
+button.addEventListener('click', function(){
+    if(input.value == ""){
+        p.innerHTML = "Vous n'avez rien rentré dans l'input";
+    }
+    else{
+        p.innerHTML = "";
+        for(i = 0; i < input.value.length; i++){
+            if(min.test(input.value[i])){
+                p.innerHTML += input.value[i].toUpperCase();
+            }
+            else if(maj.test(input.value[i])){
+                p.innerHTML += input.value[i].toLowerCase();
+            }
+            else{
+                p.innerHTML += input.value[i];
+            }
+        }
+    }
+});

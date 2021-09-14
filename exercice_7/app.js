09fb0e1671319c74e6e85d6bd1ef6ffc3f99ef3d
“CDylan-source” <chapuis.dylan18@gmail.com>
2021-09-14 16:40:34 +0200

    Style presque définitif pour la version mobile

--- exercice_7/app.js
+++ exercice_7/app.js
@@ -1,6 +1,11 @@
 window.addEventListener('load', function(){
-    document.querySelector('div').className = "deroule";
+    document.querySelector('body>div').className = "deroule";
     document.querySelector('h1').className = "deroule_h1";
     document.querySelector('p').className = "deroule_p";
     document.querySelector('p:nth-of-type(2)').className = "deroule_p";
+    document.querySelector('p:nth-of-type(3)').className = "deroule_p";
+    document.querySelector('form').className = "deroule_form";
 });
+
+const p = document.querySelector('form p');
+const form = document.querySelector('form');

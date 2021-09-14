debee68f0058b2dfb5710ee3ff8b195d8f862f4f
“CDylan-source” <chapuis.dylan18@gmail.com>
2021-09-14 15:54:38 +0200

    Début de style pour la version mobile

---
+++ exercice_7/app.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+    document.querySelector('div').className = "deroule";
+    document.querySelector('h1').className = "deroule_h1";
+    document.querySelector('p').className = "deroule_p";
+    document.querySelector('p:nth-of-type(2)').className = "deroule_p";
+});
